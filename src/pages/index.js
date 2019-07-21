92f8f67f2b2b50f706fa89480a2e667131d34127
andrew.popovici <andrew.popovici9@gmail.com>
2019-07-21 16:36:37 +0300

    -

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 /*eslint-disable */
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 /*eslint-enable */
+import MediaQuery from 'react-responsive';
 import Layout from '../components/layout/Layout';
 import Scroller from '../components/layout/Scroller';
 import Masthead from '../components/header/Masthead';
@@ -16,8 +17,11 @@ import Venue from '../components/venue/Venue';
 type PropsType = { location: Object };
 
 const HomePage = (props: PropsType): React.Node => (
-  <React.Fragment>
-    {/* <Scroller /> */}
+  <div>
+    <MediaQuery minWidth={768}>
+      <Scroller />
+    </MediaQuery>
+
     <Layout location={props.location.pathname}>
       <Element name="home">
         <Masthead />
@@ -41,7 +45,7 @@ const HomePage = (props: PropsType): React.Node => (
         <BlogPreview />
       </Element>
     </Layout>
-  </React.Fragment>
+  </div>
 );
 
 export default HomePage;
